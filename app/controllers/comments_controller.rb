42f85b46e4cb84907a65dcb4d4204ddce0bb6d8d
Vitaly Tarasenko <vitaliyt@interlink-ua.com>
2015-06-24 22:02:26 +0300

    redirect to post after comment creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,8 +4,7 @@ class CommentsController < ApplicationController
   def create
     @post = Post.take(category: params[:post][:category], id: params[:post][:id])
     @post.add_to_comments(params[:comment])
-    render 'posts/show'
-
+    redirect_to post_path(id: @post.id, category: @post.category)
   end
 
 end
